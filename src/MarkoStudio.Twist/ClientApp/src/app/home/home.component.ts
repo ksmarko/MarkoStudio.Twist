a2899071cc3b5989fbeeb30f8da5d57b251488da
Oksana Serhiienko <oksana.serhiienko@ihsmarkit.com>
2020-05-10 21:32:54 +0300

    Base FE

--- src/MarkoStudio.Twist/ClientApp/src/app/home/home.component.ts
+++ src/MarkoStudio.Twist/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  public profileStatistics: ProfileStatistics;
+
+  private baseUrl: string;
+
+  constructor(
+    private httpClient: HttpClient,
+    @Inject('BASE_URL') baseUrl: string
+  ) {
+    this.baseUrl = baseUrl;
+  }
+
+  public getProfileStatistics(userName: string) : void {
+
+    var url = this.baseUrl + `api/statistics/profile?userName=${userName}`;
+
+    this.httpClient.get<ProfileStatistics>(url).subscribe(result => {
+      this.profileStatistics = result;
+
+      console.log(result);
+    }, error => console.error(error));
+  }
+}
+
+interface ProfileStatistics{
+  sentimentScore: SentimentScore,
+  toxicityScore: ToxicityScore,
+  records: Statistics[]
+}
+
+interface SentimentScore {
+  score: Map<string, number>,
+  label: string
+}
+
+interface ToxicityScore {
+  score: number,
+  label: string
+}
+
+interface Statistics {
+  text: string,
+  sentimentScore: SentimentScore,
+  toxicityScore: ToxicityScore
 }
